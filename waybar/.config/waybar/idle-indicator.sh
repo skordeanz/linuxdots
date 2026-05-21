557cb37dd96098a55741f53413bbfe6203eab1a7
skordean <skordeanz@gmail.com>
2026-05-21 18:13:13 +0530

    change waybar modules

--- waybar/.config/waybar/idle-indicator.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if pgrep -x hypridle >/dev/null; then
-  echo '{"text": "󱫖", "tooltip": "Idle: enabled - click to disable", "class": ""}'
-else
-  echo '{"text": "󱫖", "tooltip": "Idle: disabled - click to enable", "class": "active"}'
-fi
